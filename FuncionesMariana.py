338201109f059b36e16464d950a6b1184d07ce78
smrr197 <smrr197@gmail.com>
2022-06-04 13:32:58 -0600

    cambios1

--- FuncionesMariana.py
+++ FuncionesMariana.py
@@ -9,3 +9,7 @@ import pygame #Se va a hacer uso de pygame para programar el juego
 import os #Esta libreria se usa para obtener las imagenes del sistema operativo
 
 #Funciones Mariana :) 
+def derecha (): 
+    return pygame.K_RIGHT
+def izquierda ():
+    return pygame.K_LEFT
