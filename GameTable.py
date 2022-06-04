0bf902d8c5a7afbe008db0079dcaa3fb4f53c8d6
arty4325 <arturo.acuna.duran@gmail.com>
2022-06-04 10:11:29 -0600

    Se agregaron algunas cosas al tablero

--- GameTable.py
+++ GameTable.py
@@ -1,7 +1,28 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon May 30 15:59:12 2022
-
-@author: RYZEN
+Segundo proyecto taller de Programacion
+Rojas Rojas Mariana 
+Acuña Duran Oscar
 """
 
+import pygame #Se va a hacer uso de pygame para programar el juego
+import os #Esta libreria se usa para obtener las imagenes del sistema operativo 
+
+
+
+WIN = pygame.display.set_mode((1600, 800))
+FPS = 60
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
